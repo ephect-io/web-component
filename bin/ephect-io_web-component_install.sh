db2906162765f29ae92220a77b5a910049bb9f3f
dpjb <dpjb@hotmail.fr>
2024-07-29 10:54:00 +0200

    Fix install script

--- bin/ephect-io_web-component_install.sh
+++ bin/ephect-io_web-component_install.sh
@@ -3,11 +3,4 @@ REMOVE=$1;
 
 cd vendor/ephect-io/web-component
 
-if [ "$REMOVE" = "-r" ];
-then
-  php use remove:plugin $(pwd)
-else
-  php use install:plugin $(pwd)
-fi
-
-exit 0;
+php use install:plugin $(pwd) $REMOVE
