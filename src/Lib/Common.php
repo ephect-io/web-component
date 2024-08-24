5cda63f6c712d17ce67e67a3332a865a3f328e75
dpjb <dpjb@hotmail.fr>
2024-08-24 11:06:12 +0200

    Add a Common function to retrieve the Module conf dir.

--- src/Lib/Common.php
+++ src/Lib/Common.php
@@ -17,10 +17,15 @@ class Common
         return  dirname(__DIR__) . DIRECTORY_SEPARATOR;
     }
 
+    public static function getModuleConfDir()
+    {
+        return  dirname(__DIR__, 2) . DIRECTORY_SEPARATOR . REL_CONFIG_DIR;
+    }
+
     public static function getModuleManifest(): ModuleManifestEntity
     {
         $manifestReader = new ModuleManifestReader();
-        return $manifestReader->read(Common::getModuleDir());
+        return $manifestReader->read(Common::getModuleConfDir());
     }
 
     public static function getCustomWebComponentRoot(): string
